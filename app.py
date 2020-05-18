53af93fe039ee2de2bf64fdaa38a6a70ac81fe95
MadhabaPatra <madhabapatra@gmail.com>
2020-05-17 23:04:06 -0700

    commited first

---
+++ app.py
@@ -0,0 +1,34 @@
+import flask
+import pafy
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+
+def Get(url):
+    myvid = pafy.new(url)
+    best = myvid.getbest()
+    response = [
+    {'id': url,
+     'youtube':"https://www.youtube.com/watch?v="+url,
+     'download': best.url,
+     },]
+    return jsonify(response)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>My Tube Downloader</h1>
+<p>An API To Generate Download Link for your youtube link.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/download/<VIDEO_ID>', methods=['GET'])
+def download(VIDEO_ID):
+    return Get(VIDEO_ID)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="localhost", port=8081)
+app.run()
